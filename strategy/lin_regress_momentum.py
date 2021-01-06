c7b21363164455deb98ad48d87ee961a716ec4ae
Umoh Bassey-Duke <umohduke@gmail.com>
2021-01-06 22:12:54 +0100

    Removing future imports

--- strategy/lin_regress_momentum.py
+++ strategy/lin_regress_momentum.py
@@ -1,6 +1,3 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
 import pandas as pd
 from os import listdir
 from os.path import isfile, join, dirname, basename
